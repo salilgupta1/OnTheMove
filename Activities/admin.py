008b8fe08712a791dbad3cdf19bb7458b0887d5a
Brittany <leeabrittany@gmail.com>
2014-05-08 17:19:03 -0500

    added activity, location and user data

--- Activities/admin.py
+++ Activities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from Activities.models import OnthemoveActivity, OnthemoveLocation
 
 # Register your models here.
+admin.site.register(OnthemoveActivity)
+admin.site.register(OnthemoveLocation)
