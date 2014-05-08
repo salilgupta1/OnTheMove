ebf077dd018c2a8152d84d57bb8aab6783608257
GregChan <gregorywarrenchan@gmail.com>
2014-05-08 16:31:09 -0500

    started details

--- Activities/views.py
+++ Activities/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+# from Activites.model import OnthemoveActivity, OnthemoveLocation
 
 # Create your views here.
 
-def details(request):
-	return render(request,'Activities/details.html')
+def details(request, id):
+	activity = OnthemoveActivity.objects.get(activity_id=id)
+	context = {}
+	context["Activity"] = 
+	return render(request,"Activities/details.html", context)
