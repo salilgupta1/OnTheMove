09884cd84d6a8ef752e03c93b2e49670036acd7d
wade2999 <wade2999lpc@gmail.com>
2014-05-08 17:01:46 -0500

    edit urls and templates for views

--- Activities/urls.py
+++ Activities/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Onthemove.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^details(?P<activity_id>[\w]{8}(-[\w]{4}){3}-[\w]{12})/$',views.details, name='details'),
+    url(r'^details/(?P<id>\d)/$',views.details, name='details'),
 )
--- Activities/views.py
+++ Activities/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-# from Activites.model import OnthemoveActivity, OnthemoveLocation
+from Activities.models import OnthemoveActivity as Act, OnthemoveLocation as Loc
 
 # Create your views here.
 
 def details(request, id):
-	activity = OnthemoveActivity.objects.get(activity_id=id)
+	activity = Act.objects.get(activity_id=id)
 	context = {}
-	context["Activity"] = 
+	context["activity_name"] = activity.activity_name
+	context["start_time"] = activity.start_time
+	location = Loc.objects.get(location_id = activity.location_id)
+	context["location_name"] = location.location_name
+	context["date"] = activity.date #not work
+
+
 	return render(request,"Activities/details.html", context)
