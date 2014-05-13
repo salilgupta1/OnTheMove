f4276d8dce697edea95fa8e04ad1337967b7e300
GregChan <gregorywarrenchan@gmail.com>
2014-05-13 11:24:38 -0500

    added views and details removed db.sqlite3

--- Activities/views.py
+++ Activities/views.py
@@ -8,9 +8,9 @@ def details(request, id):
 	context = {}
 	context["activity_name"] = activity.activity_name
 	context["start_time"] = activity.start_time
-	location = Loc.objects.get(location_id = activity.location_id)
+	location = activity.location_id
 	context["location_name"] = location.location_name
-	context["date"] = activity.date #not work
+	context["date"] = activity.date.strftime('%m/%d/%Y') #not work
 
 
 	return render(request,"Activities/details.html", context)
