283e52116e0e04ecfa1ab4de559bd7420bdd8fa1
salilgupta1 <salil.gupta323@gmail.com>
2014-05-12 22:25:59 -0500

    Actually secured settings files

--- Onthemove/settings.py
+++ Onthemove/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os,config
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = config.keys['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,8 +90,8 @@ STATICFILES_DIRS= (
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_USER = config.keys['SENDGRID_USERNAME']
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_PASSWORD = config.keys['SENDGRID_PASSWORD']
--- Onthemove/settings_prod.py
+++ Onthemove/settings_prod.py
@@ -17,3 +17,10 @@ DATABASES['default'] =  dj_database_url.config()
 INSTALLED_APPS = INSTALLED_APPS + (
     'raven.contrib.django.raven_compat',
 )
+
+import os
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
