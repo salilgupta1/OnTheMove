124a56572fe5232c14a4168447266e14187cd934
salilgupta1 <salil.gupta323@gmail.com>
2014-05-14 00:54:43 -0500

    Settings redo

--- Onthemove/wsgi.py
+++ Onthemove/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Onthemove.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Onthemove.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
