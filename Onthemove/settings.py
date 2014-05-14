f06523b4b26fc8341982f6f20ba402d726d4ecc0
salilgupta1 <salil.gupta323@gmail.com>
2014-05-13 19:43:03 -0500

    changed db name

--- Onthemove/settings.py
+++ Onthemove/settings.py
@@ -60,9 +60,13 @@ WSGI_APPLICATION = 'Onthemove.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "onthemove",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
