d25101a2c25c358411a82a77ab6c3dec476f82b9
salilgupta1 <salil.gupta323@gmail.com>
2014-05-15 02:42:18 -0500

    js file for Activities page

---
+++ Activities/static/Activities/js/Activities.js
@@ -0,0 +1,32 @@
+var Activities = (function($){
+	var enroll = function(data,path){
+		$("#enroll").click(function(){
+			$("#loading").show();
+			$.ajax({
+				type: "POST",
+				dataType:"text",
+				data:data,
+				url:path,
+				success:function(response){
+					$("#loading").hide();
+					$('#enroll-modal').modal('hide');
+					$("#enroll-list-group").show();
+					$("#enrollSuccess").fadeIn('3000');
+					$('#enrollSuccess').delay(3000).fadeOut('3000');
+					
+				},
+				error:function(error){
+					console.log(error);
+				}
+			});
+		});
+	},
+	init = function(){
+		$("#enroll-list-group").hide();
+		$("#loading").hide();
+	};
+	return {
+		enroll:enroll,
+		init:init
+	};
+}(jQuery));
