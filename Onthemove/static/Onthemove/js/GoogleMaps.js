71a66b62fc11cea7190b441a3764a34233192c81
Brittany <leeabrittany@gmail.com>
2014-05-15 23:08:15 -0500

    made map infowindow dynamic in size

--- Onthemove/static/Onthemove/js/GoogleMaps.js
+++ Onthemove/static/Onthemove/js/GoogleMaps.js
@@ -23,9 +23,10 @@ var GoogleMap = (function($){
 		for(var i = locl-1;i>=0;i--){
 			var aLoc = $.parseJSON(loc[i]);
 			var url = "activities/details/"+act[i]['pk'];
-			var content = "<div>"+act[i]['fields']['activity_name']+"</div><a href="+url+">View Details</a>";
+			var content_str = "<div>"+act[i]['fields']['activity_name']+"</div><a href="+url+">View Details</a>";
 
-			var infowindow = new google.maps.InfoWindow();
+			var infowindow = new google.maps.InfoWindow({content: content_str});
+			// infowindow.setContent(content);
 			var coods = new google.maps.LatLng(aLoc[0]['fields']['latitude'],aLoc[0]['fields']['longitude']);
 			var marker = new google.maps.Marker({
 				position:coods,
@@ -34,8 +35,8 @@ var GoogleMap = (function($){
 			marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
 			google.maps.event.addListener(marker,'click',(function(marker,content,infowindow){ 
         		return function() {
-		           infowindow.setContent(content);
-		           infowindow.open(map,marker);
+		        	
+		        	infowindow.open(map,marker);
 		        };
    			})(marker,content,infowindow)); 
 		}
