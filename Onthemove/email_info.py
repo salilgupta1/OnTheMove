1ebd40924a83e5558fbfb9c291f3f6b547c05a9f
wade2999 <wade2999lpc@gmail.com>
2014-05-27 16:36:26 -0500

    search location done

---
+++ Onthemove/email_info.py
@@ -0,0 +1,8 @@
+# for gmail or google apps
+DEFAULT_FROM_EMAIL = 'wade2999lpc@gmail.com'
+SERVER_EMAIL = 'wade2999lpc@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'wade2999lpc@gmail.com'
+EMAIL_HOST_PASSWORD = 'wade29998256063'
+EMAIL_PORT = 587
--- Onthemove/static/Onthemove/js/GoogleMaps.js
+++ Onthemove/static/Onthemove/js/GoogleMaps.js
@@ -10,6 +10,65 @@ var GoogleMap = (function($){
 			position:coords,
 			map:mapA
 		}),
+
+
+
+  input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+
+  // mapA.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo('bounds', mapA);
+
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: mapA,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      mapA.fitBounds(place.geometry.viewport);
+    } else {
+      mapA.setCenter(place.geometry.location);
+      mapA.setZoom(17);  // Why 17? Because it looks good.
+    }
+    marker.setIcon(/** @type {google.maps.Icon} */({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35)
+    }));
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+      ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(mapA, marker);
+  });
+
+
+
 		infowindow = new google.maps.InfoWindow({
 			content: "You are here!"
 		});
